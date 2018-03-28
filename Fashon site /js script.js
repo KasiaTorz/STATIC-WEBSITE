88da594304abb89b7d3117c62658a0320ae2941a
KasiaTorz <ktorz@wp.pl>
2018-03-28 13:59:51 +0200

    add statements

--- Fashon site /js script.js
+++ Fashon site /js script.js
@@ -1,3 +1,17 @@
+function toggleCB(cb){
+    if (cb.dataset.checked == 0){
+        cb.style.background = "#89D814";
+        cb.dataset.checked =1;
+    }
+    else {
+        cb.style.background = "#EEE";
+        cb.dataset.checked = 0;
+    }
+}
+
+
+
+
 function submitValues(){
     alert ("cb1=  "+cb1.dataset.checked+"  cb2 = "+cb2.dataset.checked);
 }
